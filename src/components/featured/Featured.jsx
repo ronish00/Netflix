460d0aa54367e71325f32a662c9f5a8e5ebedd67
ronishkatuwal00 <ronishkatuwal00@gmail.com>
2024-06-15 15:45:04 +0545

    navbar done

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,14 @@
+import "./featured.scss";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <img
+        src="https://www.denofgeek.com/wp-content/uploads/2021/02/Behind-Her-Eyes-Netflix-poster-landscape.jpg?fit=1200%2C675"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Featured;
